2f67c386ca73c1a956ad69748bc850480df5937e
jimmy <jimmy@pixnet.tw>
2017-02-03 17:52:08 +0800

    Use vue-router to do SPA

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+
+// init
+Vue.use(VueRouter)
+
+// page
+import Hello from './pages/Hello.vue';
+import CtoF from './pages/CtoF.vue';
+import App from './App.vue';
+
+const router = new VueRouter({
+  // 使用 HTML 5 模式
+  mode: 'history',
+  base: __dirname,
+  // routre 表
+  routes: [
+    {
+      path: '/hello',
+      name: 'hello',
+      component: Hello
+    },
+    {
+      path: '/c2f',
+      name: 'c2f',
+      component: CtoF
+    },
+    // router 轉址
+    { path: '/*', redirect: '/hello' }
+  ]
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  // router 掛載設定
+  router,
+  // app.vue 掛載並 replace index.html 原始掛載點： <div id="app"></div>
+  render: h => h( App )
 })
