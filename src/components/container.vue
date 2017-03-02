9f7a8d3e7447e9b133edabeb925ba449a546c6eb
jimmy <jimmy@pixnet.tw>
2017-03-02 10:57:09 +0800

    Add page and build container

---
+++ src/components/container.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4" v-for="item in list">
+			  <h2>{{ item.title }}</h2>
+			  <p>{{ item.info }}</p>
+			  <p><a class="btn btn-default" href="#" role="button">{{ item.btntext }}</a></p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  // vue option 當中有 props 開發區域
+  props: {
+    // attribute name: Type
+    list: Array
+  },
+  //data () {
+  //  return {
+  //    list: [ 資料"不"需要存在這邊了 ],
+  //  }
+  //},
+}
+</script>
+<style>
+	
+</style>
---
+++ src/pages/LearnStructure.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <navbar />
+    <container :list="list" />
+    <jumbotron />
+    <!-- <Hello/> -->
+  </div>
+</template>
+
+<script>
+// import NavBar from '../components/navbar.vue'
+// import Jumbotron from '../components/jumbotron.vue'
+import Container from '../components/container.vue'
+// 可以嘗試把昨天以前的範例載入這個新頁面使用看看！
+import Hello from './Hello.vue'
+
+export default {
+  data () {
+
+    return {
+      list: [
+        {
+          title: 'Heading',
+          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+          btntext: 'View details »'
+        },
+        {
+          title: 'Heading',
+          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+          btntext: 'View details »'
+        },
+        {
+          title: 'Heading',
+          info: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+          btntext: 'View details »'
+        },  
+      ],
+    }
+
+  },
+
+  components: {
+    // Navbar,
+    // Jumbotron,
+    Container,
+    Hello
+  }
+}
+</script>
+<style>
+
+</style>
