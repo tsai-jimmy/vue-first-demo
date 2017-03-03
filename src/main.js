3fe6ba98ff8ed209901320d36a8ac622ad707d1f
jimmy <jimmy@pixnet.tw>
2017-03-03 18:37:01 +0800

    implement the counter ,and Use vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 引入 store 資料夾（上面五隻 js ）
+// 預設會去找 index.js  如果沒有的話會報錯！
+// 我們在上一個範例已經組合所有指揮挺了！！
+import store from './store'
+
 // 引入 vuex
 import Vuex from 'vuex'
 
@@ -16,6 +21,7 @@ import Hello from './pages/Hello.vue';
 import CtoF from './pages/CtoF.vue';
 import CtoFstep2 from './pages/CtoF-step2.vue';
 import LearnStructure from './pages/LearnStructure.vue';
+import Count from './pages/count.vue';
 import App from './App.vue';
 
 const router = new VueRouter({
@@ -52,6 +58,11 @@ const router = new VueRouter({
       name: 'learnstructure',
       component: LearnStructure
     },
+    {
+      path: '/count',
+      name: 'count',
+      component: Count
+    },
     // router 轉址
     { path: '/*', redirect: '/hello' }
   ]
@@ -62,6 +73,10 @@ new Vue({
   el: '#app',
   // router 掛載設定
   router,
+
+  // 加入 store
+  store,
+
   // app.vue 掛載並 replace index.html 原始掛載點： <div id="app"></div>
   render: h => h( App )
 })
---
+++ src/store/getters.js
@@ -0,0 +1,4 @@
+export const getCount = state => { 
+	// console.log(state.count);
+	return state.count 
+}
